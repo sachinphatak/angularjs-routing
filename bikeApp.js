b53fec9cc12d296d19cc23713c04fa7eddc871b5
Sachin <sachin.phatak92@gmail.com>
2017-12-05 16:28:11 +0530

    ui-routing fix

--- bikeApp.js
+++ bikeApp.js
@@ -1,19 +1,8 @@
-angular.module('bikeApp', ['showListApp','descViewApp','ui.router']);
+angular.module('bikeApp', ['showListApp', 'descViewApp', 'ui.router']);
 
 angular.module('bikeApp')
-.controller('bikeParent',['$scope','BikeList', function($scope,BikeList) {
-    $scope.showList = true;   
-    $scope.item = undefined;
+.controller('bikeParent',['$scope','BikeList', '$rootScope', '$state', function($scope, BikeList, $rootScope, $state) {
     $scope.bikeList = BikeList.bikeList;
-    console.log($scope.bikeList);
-
-    $scope.bikeToShow = function (item) {
-        $scope.item = item;
-        $scope.showList = !$scope.showList;
-    }
-    $scope.onBack = function () {
-        $scope.showList = !$scope.showList;
-    }    
 }])
 .factory('BikeList',[function () {
     var list = [
@@ -33,27 +22,41 @@ angular.module('bikeApp')
     return {
         bikeList: list
     };
+}])
+.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+    .state('root', {
+        template: `
+            <show-list item-clicked='actionOnClick(item)' bike-list='bikeList'></show-list>
+        `,
+        url: '/',
+        controller: ['$scope', '$state', 'BikeList', function ($scope, $state, BikeList) {
+            $scope.actionOnClick = function (clickedItem) {
+                $state.go('listDetailView', {url: clickedItem.name});
+            };
+            $scope.bikeList = BikeList.bikeList;
+        }]
+    })
+    .state('listDetailView', {
+        template: `
+        <desc-view show='currentBike' onback='onBack()' show-back='true'>
+        `,
+        url: '/{url:string}',
+        resolve: {
+            currentBike: ['BikeList', '$stateParams', function (BikeList, $stateParams) {
+                for (var i = 0; i < BikeList.bikeList.length; ++i) {
+                    if (BikeList.bikeList[i].name === $stateParams.url) {
+                        return BikeList.bikeList[i];
+                    }
+                }
+            }]
+        },
+        controller: ['$scope', 'currentBike', '$state', function ($scope, currentBike, $state) {
+            $scope.currentBike = currentBike;
+            $scope.onBack = function () {
+                $state.go('root');
+            };
+        }]
+    })
 }]);
-
-// .config(['$stateProvider', function ($stateProvider) {
-//     $stateProvider.state("listDetailView", {
-//         template:"{{description}}",
-//         url:'/{url:string}',
-//         controller: 'detailViewController', //register on same module
-//         resolve: {
-//             desc: ['BikeList','$stateParams', function (BikeList, $stateParams) {
-//                 BikeList.foreach(function (bike) {
-//                     console.log(bike.name);
-//                     if(bike.name === $stateParams.url) 
-//                         return bike.desc;
-//                 })
-//             }]
-//         }
-//     })
-// }])
-// .controller('detailViewController', ['$scope','desc', function($scope, desc) {
-//     $scope.description = desc;
-//     console.log(desc);
-// }]);
-
 
